14c9868a31668c5ff0548b800671cabc45494a5c
Katie Davies <millerkatie1990@gmail.com>
2024-09-06 10:17:45 +1200

    update bg colour

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
-    <div className="flex justify-center">
-      <h1>Project Manager</h1>
+    <div className="flex justify-center bg-blue-500">
+      <h1 className="text-white font-bold">Project Manager</h1>
     </div>
   )
 }
