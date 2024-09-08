d71e721b3cdd651503a2b8707d00b2246fc747a4
Katie Davies <millerkatie1990@gmail.com>
2024-09-08 18:59:14 +1200

    styling input

--- Frontend/src/pages/CreateProject.tsx
+++ Frontend/src/pages/CreateProject.tsx
@@ -1,13 +1,28 @@
 function CreateProject() {
   console.log('createproject')
   return (
-    <div className="flex flex-grow">
-      <h1>Create Project</h1>
+    <div className="flex flex-grow content-center  flex-col flex-wrap">
+      <h1 className="text-4xl m-5">Create a Project</h1>
       <div>
-        <form>
-          <input type="text" placeholder="Project name" />
-          <input type="text" placeholder="Project Owner" />
-          <input type="text" placeholder="Budget" />
+        <form className="flex flex-col">
+          <label htmlFor="projectName">PROJECT NAME</label>
+          <input
+            type="text"
+            placeholder="Enter Name"
+            className="m-3 border-solid border-2 border-gray-300"
+          />
+          <label htmlFor="projectOwner">PROJECT OWNER</label>
+          <input
+            type="text"
+            placeholder="Project Owner"
+            className="m-3 border-solid border-2 border-gray-300"
+          />
+          <label htmlFor="ProjectBudget">BUDGET</label>
+          <input
+            type="text"
+            placeholder="Budget"
+            className="m-3 border-solid border-2 border-gray-300"
+          />
         </form>
       </div>
     </div>
