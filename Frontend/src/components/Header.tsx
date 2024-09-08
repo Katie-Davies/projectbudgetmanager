466caafc617201b9c2fe6c23d8feb8ff4bf32ca9
Katie Davies <millerkatie1990@gmail.com>
2024-09-08 19:34:01 +1200

    hide nav if on homepage

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,15 +1,20 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 function Header() {
+  const location = useLocation()
+
   return (
     <div className="flex justify-start bg-customBlue h-16 content-center flex-wrap">
-      <Link
-        className="text-white font-bold text-2xl ml-5"
-        to=".."
-        relative="path"
-      >
-        • • •
-      </Link>
+      {location.pathname === '/createproject' ||
+      location.pathname === '/log' ? (
+        <Link
+          className="text-white font-bold text-2xl ml-5"
+          to=".."
+          relative="path"
+        >
+          • • •
+        </Link>
+      ) : null}
     </div>
   )
 }
