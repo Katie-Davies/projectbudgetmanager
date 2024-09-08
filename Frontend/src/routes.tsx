fe2ce3bc030fd47eb223f9521dee9a8c306b4505
Katie Davies <millerkatie1990@gmail.com>
2024-09-08 19:36:12 +1200

    add to routes

--- Frontend/src/routes.tsx
+++ Frontend/src/routes.tsx
@@ -3,11 +3,13 @@ import App from './pages/App'
 import CreateProject from './pages/CreateProject'
 import LogPage from './pages/LogPage'
 import Layout from './pages/Layout'
+import AllProjects from './pages/AllProjects'
 
 export default createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route index element={<App />} />
     <Route path="/log" element={<LogPage />} />
+    <Route path="/allprojects" element={<AllProjects />} />
     <Route path="/createproject" element={<CreateProject />} />
   </Route>
 )
