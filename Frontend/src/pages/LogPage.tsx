f701856f7628a0cd20bddaaaafc009b5240d7ed2
Katie Davies <millerkatie1990@gmail.com>
2024-09-08 19:41:39 +1200

    add drop down selection

--- Frontend/src/pages/LogPage.tsx
+++ Frontend/src/pages/LogPage.tsx
@@ -1,8 +1,19 @@
 function LogPage() {
+  const fakeData = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
   console.log('logpage')
   return (
     <div className="flex flex-grow">
       <h1>LogPage</h1>
+      <form>
+        <label htmlFor="dropwdown"> Select your project:</label>
+        <select name="dropdown" id="dropdown">
+          <option value="">--Select an option--</option>
+          {fakeData.map((data) => (
+            <option value={data}>{data}</option>
+          ))}
+        </select>
+      </form>
     </div>
   )
 }
