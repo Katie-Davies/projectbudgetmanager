db9f8cfd74093033b3db9ce10ff8a01580946bd8
Katie Davies <millerkatie1990@gmail.com>
2024-09-08 19:01:07 +1200

    update placeholders

--- Frontend/src/pages/CreateProject.tsx
+++ Frontend/src/pages/CreateProject.tsx
@@ -5,22 +5,28 @@ function CreateProject() {
       <h1 className="text-4xl m-5">Create a Project</h1>
       <div>
         <form className="flex flex-col">
-          <label htmlFor="projectName">PROJECT NAME</label>
+          <label htmlFor="projectName" className="ml-3">
+            PROJECT NAME
+          </label>
           <input
             type="text"
-            placeholder="Enter Name"
+            placeholder="Enter Project Name"
             className="m-3 border-solid border-2 border-gray-300"
           />
-          <label htmlFor="projectOwner">PROJECT OWNER</label>
+          <label htmlFor="projectOwner" className="ml-3">
+            PROJECT OWNER
+          </label>
           <input
             type="text"
-            placeholder="Project Owner"
+            placeholder="Enter project Owner"
             className="m-3 border-solid border-2 border-gray-300"
           />
-          <label htmlFor="ProjectBudget">BUDGET</label>
+          <label htmlFor="ProjectBudget" className="ml-3">
+            BUDGET
+          </label>
           <input
             type="text"
-            placeholder="Budget"
+            placeholder="Enter Budget"
             className="m-3 border-solid border-2 border-gray-300"
           />
         </form>
