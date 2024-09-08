62163eed43902a5fe4ca89ab73fc48b1a293a6ec
Katie Davies <millerkatie1990@gmail.com>
2024-09-08 18:51:56 +1200

    link to home page

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom'
+
 function Header() {
   return (
     <div className="flex justify-center bg-blue-500 h-16 content-center flex-wrap">
-      <h1 className="text-white font-bold text-2xl">Project Manager</h1>
+      <Link className="text-white font-bold text-2xl" to=".." relative="path">
+        Project Manager
+      </Link>
     </div>
   )
 }
