227973583076e3b6f397105db2d43087d6ccdf50
Katie Davies <millerkatie1990@gmail.com>
2024-09-09 13:54:59 +1200

    style border

--- Frontend/src/pages/AllProjects.tsx
+++ Frontend/src/pages/AllProjects.tsx
@@ -1,19 +1,22 @@
 function AllProjects() {
   return (
     <div>
-      <h1>All Projects</h1>
-      <table className="table-auto">
-        <thead>All Projects</thead>
-        <tr>
-          <td>project name </td>
-          <td>project owner</td>
-          <td>budget</td>
-        </tr>
-        <tr>
-          <td>Disney </td>
-          <td>Kate</td>
-          <td>100,000</td>
-        </tr>
+      <h1 className="text-4xl">All Projects</h1>
+      <table className="table-auto border-collapse border border-slate-400 ">
+        <thead>
+          <tr>
+            <th className="border border-slate-300 p-3">PROJECT NAME </th>
+            <th className="border border-slate-300 p-3">PROJECT OWNER</th>
+            <th className="border border-slate-300 p-3">BUDGET</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="border border-slate-300 ">Disney </td>
+            <td className="border border-slate-300 ">Kate</td>
+            <td className="border border-slate-300 ">100,000</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   )
