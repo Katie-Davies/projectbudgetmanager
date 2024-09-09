59adb128e9bc68943c4d7d77b86fa73c470135ef
Katie Davies <millerkatie1990@gmail.com>
2024-09-09 19:21:06 +1200

    add api request to put and post

--- Frontend/src/api/apiClient.ts
+++ Frontend/src/api/apiClient.ts
@@ -7,3 +7,14 @@ export async function getAllProjects() {
   const projects = await request.get(`${api}/projects`)
   return projects.body as IProject[]
 }
+
+export async function creatProject(project: IProject) {
+  return await request.post(`${api}/projects`).send(project)
+}
+
+export async function updateProject(project: IProject) {
+  const updatedProject = await request
+    .put(`${api}/projects/${project.projectId}`)
+    .send(project)
+  return updatedProject.body as IProject
+}
