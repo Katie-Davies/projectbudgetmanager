2e2bb5b7d37982bdda7f818ab4e779e3aa2f802e
Katie Davies <millerkatie1990@gmail.com>
2024-09-09 18:43:52 +1200

    add Cors policy

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -3,12 +3,25 @@ using System.Linq;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy("AllowReactApp",
+  builder =>
+  {
+    builder.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod();
+  });
+});
 
 var app = builder.Build();
+
+//use CORS policy
+app.UseCors("AllowReactApp");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
