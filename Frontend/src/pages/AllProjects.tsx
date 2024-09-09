fa90634b16280355ba17e21a0c3a9a441ef62f12
Katie Davies <millerkatie1990@gmail.com>
2024-09-09 13:48:26 +1200

    create fake table

--- Frontend/src/pages/AllProjects.tsx
+++ Frontend/src/pages/AllProjects.tsx
@@ -2,6 +2,19 @@ function AllProjects() {
   return (
     <div>
       <h1>All Projects</h1>
+      <table>
+        <thead>All Projects</thead>
+        <tr>
+          <td>project name </td>
+          <td>project owner</td>
+          <td>budget</td>
+        </tr>
+        <tr>
+          <td>Disney </td>
+          <td>Kate</td>
+          <td>100,000</td>
+        </tr>
+      </table>
     </div>
   )
 }
