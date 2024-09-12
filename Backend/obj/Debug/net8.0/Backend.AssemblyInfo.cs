2331d7defa596c3f5458d5a333143e3af69bd3b5
Sukhjeet Chauhan <chauhansukhjeet@gmail.com>
2024-09-12 21:34:26 +1200

    add createProject handleSubmit

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9935d4b996162d977f6f60bfd19e5c9bd068c54f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c5b69cca055c0c708d2574e2fb489a570962085")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
